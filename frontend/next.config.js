1266ffd1aa293e4409ee3377a77767fd809ef2c5
Nandakishor2206 <nandakishorchandolu11@gmail.com>
2025-04-20 12:52:41 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig;
-  
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
+};
